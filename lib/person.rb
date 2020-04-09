3f0ff8fdb945c679b3e453d2397cc4e5048880b1
Micah McCallum <micahmccallum@outlook.com>
2020-04-09 03:34:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,6 @@ class Person
   attr_accessor :name
 
   def initialize(attributes)
-    attributes.each {|key, value| self.send (("#{key}="), value)}
+    attributes.each {|key, value| self.send ("#{key}=", value)}
   end
 end
